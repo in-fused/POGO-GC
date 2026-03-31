36bd1b410359f0bfa0323c8b76a67c60ca9e720e
Infused <infusednft@gmail.com>
2026-03-31 00:13:56 -0400

    Update ably.js
    Fix

--- js/ably.js
+++ js/ably.js
@@ -1,31 +1,27 @@
-let ablyInstance = null;
-let channelInstance = null;
+let ably = null;
+let channel = null;
 
 async function initializeAbly(username, team, group) {
-    if (ablyInstance) return channelInstance;
+    if (ably) return channel;
 
-    ablyInstance = new Ably.Realtime({ key: window.APP_CONFIG.ABLY_KEY, clientId: username });
+    ably = new Ably.Realtime({ key: window.APP_CONFIG.ABLY_KEY, clientId: username });
 
-    await new Promise(resolve => ablyInstance.connection.once('connected', resolve));
+    await new Promise(resolve => ably.connection.once('connected', resolve));
 
     const channelName = `poke-raid-${group}`;
-    channelInstance = ablyInstance.channels.get(channelName);
+    channel = ably.channels.get(channelName);
 
-    return channelInstance;
+    return channel;
 }
 
 function subscribeToMessages(callback) {
-    if (channelInstance) channelInstance.subscribe('message', callback);
-}
-
-function subscribeToTyping(callback) {
-    if (channelInstance) channelInstance.subscribe('typing', callback);
+    if (channel) channel.subscribe('message', msg => callback(msg.data));
 }
 
 function subscribeToMapReports(callback) {
-    if (channelInstance) channelInstance.subscribe(['spawn', 'nest'], callback);
+    if (channel) channel.subscribe(['spawn', 'nest'], msg => callback(msg.data));
 }
 
 async function enterPresence(username, team) {
-    if (channelInstance) await channelInstance.presence.enter({ username, team });
+    if (channel) await channel.presence.enter({ username, team });
 }
